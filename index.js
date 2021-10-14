4e88ff7caf2c969b91d149e7c34ea977ad75ca75
Manta, Adrian <a.manta@sap.com>
2021-10-14 17:55:30 +0300

    - first working version

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 "use strict";
 
 const https = require('https')
+var parse = require('fast-json-parse')
 
 const baseUrl = "eu.salusconnect.io";
 const loginUrl = "/users/sign_in.json?";
 const apiVersion = "/apiv1";
 const devicesUrl = "/devices.json?";
 
-const atrTemperature = "LocalTemperature_x100";
-const atrHumidity = "SunnySetpoint_x100";
-const atrHeatingSetpoint = "HeatingSetpoint_x100";
-const atrRunningMode = "RunningMode";
+const prop = "ep_9:sIT600TH:";
+const propTemperature = prop + "LocalTemperature_x100";
+const propHumidity = prop + "SunnySetpoint_x100";
+const propHeatingSetpoint = prop + "HeatingSetpoint_x100";
+const propRunningMode = prop + "RunningMode";
 
 const oem_model = "SQ610";
 
@@ -19,6 +21,17 @@ class Index {
     constructor({username, password}) {
         this.username = username;
         this.password = password;
+    }
+
+    async getToken() {
+        await this.login();
+
+        function Token(value, creationDay) {
+            this.value = value;
+            this.creationDay = creationDay;
+        }
+
+        return new Token(this.token, new Date().getDate());
     }
 
     login() {
@@ -37,7 +50,7 @@ class Index {
 
             const req = https.request(options, res => {
                 res.on('data', d => {
-                    this.token = JSON.parse(d.toString()).access_token;
+                    this.token = parse(d.toString()).value.access_token;
                     resolve(this.token);
                 })
             })
@@ -49,122 +62,85 @@ class Index {
         })
     }
 
-    async getDevices() {
-        await this.login();
-        return new Promise((resolve, reject) => {
-            const options = {
-                host: baseUrl,
-                port: 443,
-                path: apiVersion + devicesUrl + this.appendTimestamp(),
-                method: 'GET',
-                headers: {
-                    'Authorization': 'Bearer ' + this.token
-                }
+    async getDevices(token) {
+        if (token !== null) {
+            const allDevices = await this.getData(token, apiVersion + devicesUrl + this.appendTimestamp());
+
+            function Item(id, name, current, target, heating) {
+                this.id = id;
+                this.name = name;
+                this.current = current;
+                this.target = target;
+                this.heating = heating;
             }
 
-            const req = https.request(options, res => {
-                res.on('data', async d => {
-                    const result = [];
+            const result = [];
 
-                    function Item(id, name, currentTemperature, targetTemperature, humidity, heating) {
-                        this.id = id;
-                        this.name = name;
-                        this.current = currentTemperature;
-                        this.target = targetTemperature;
-                        this.humidity = humidity;
-                        this.heating = heating;
+            try {
+                for (const e of allDevices.value) {
+                    const device = e.device;
+                    if (device.oem_model === oem_model) {
+                        const current = (await this.getData(token, apiVersion + "/dsns/" + device.dsn + "/properties/" + propTemperature + ".json?" + this.appendTimestamp())).value.property.value / 100;
+                        const target = (await this.getData(token, apiVersion + "/dsns/" + device.dsn + "/properties/" + propHeatingSetpoint + ".json?" + this.appendTimestamp())).value.property.value / 100;
+                        const heating = (await this.getData(token, apiVersion + "/dsns/" + device.dsn + "/properties/" + propRunningMode + ".json?" + this.appendTimestamp())).value.property.value !== 0 ? true : false;
+
+                        result.push(new Item(device.dsn, device.product_name, current, target, heating));
                     }
+                }
+                return result;
+            } catch (error) {
+                console.error(error);
+            }
 
-                    for (const e of JSON.parse(d.toString())) {
-                        const device = e.device;
-                        if (device.oem_model === oem_model) {
-                            const deviceInfo = await this.getCurrentState(device.dsn);
-                            result.push(new Item(device.dsn, device.product_name, deviceInfo.temperature, deviceInfo.heatingSetpoint, deviceInfo.humidity, deviceInfo.runningMode));
-                        }
-                    }
-                    resolve(JSON.stringify(result));
-                })
-            })
-
-            req.on('error', error => {
-                console.error(error)
-            })
-
-            req.end()
-        })
+        } else {
+            console.warn("Salus login failed");
+        }
     }
 
-    getCurrentState(id) {
-        return new Promise((resolve, reject) => {
-            if (!id)
-                throw new Error("ID argument must be set");
-            const buffers = [];
+    async getDeviceCurrentTemperature(token, id) {
+        return (await this.getData(token, apiVersion + "/dsns/" + id + "/properties/" + propTemperature + ".json?" + this.appendTimestamp())).value.property.value / 100;
+    }
 
+    async getDeviceTargetTemperature(token, id) {
+        return (await this.getData(token, apiVersion + "/dsns/" + id + "/properties/" + propHeatingSetpoint + ".json?" + this.appendTimestamp())).value.property.value / 100;
+    }
+
+    async getDeviceHeating(token, id) {
+        return (await this.getData(token, apiVersion + "/dsns/" + id + "/properties/" + propRunningMode + ".json?" + this.appendTimestamp())).value.property.value !== 0 ? true : false;
+    }
+
+
+    getData(token, path) {
+        return new Promise((resolve, reject) => {
             const options = {
                 host: baseUrl,
                 port: 443,
-                path: apiVersion + "/dsns/" + id + "/properties.json?" + this.appendTimestamp(),
+                path: path,
                 method: 'GET',
                 headers: {
-                    'Authorization': 'Bearer ' + this.token
+                    'Authorization': 'Bearer ' + token
                 }
             }
 
             const req = https.request(options, res => {
                 res.on('data', d => {
-                    buffers.push(d)
-                })
-                res.on('end', function () {
-
-                    function Item(id, displayName, temperature, humidity, heatingSetpoint, runningMode) {
-                        this.id = id;
-                        this.displayName = displayName;
-                        this.temperature = temperature;
-                        this.humidity = humidity;
-                        this.heatingSetpoint = heatingSetpoint;
-                        this.runningMode = runningMode;
-                    }
-
-                    let roomName = "";
-                    let temperature = 0;
-                    let humidity = 0;
-                    let heatingSetpoint = 0;
-                    let runningMode = false;
-
-                    JSON.parse(Buffer.concat(buffers).toString()).forEach(e => {
-                        if (e.property.display_name === atrTemperature) {
-                            temperature = e.property.value;
-                        }
-                        if (e.property.display_name === atrHumidity) {
-                            humidity = e.property.value;
-                        }
-                        if (e.property.display_name === atrHeatingSetpoint) {
-                            heatingSetpoint = e.property.value;
-                            roomName = e.property.product_name;
-                        }
-                        if (e.property.display_name === atrRunningMode) {
-                            if (e.property.value !== 0) {
-                                runningMode = true;
-                            }
-                        }
-                    });
-                    resolve(new Item(id, roomName, temperature, humidity, heatingSetpoint, runningMode));
+                    resolve(parse(d.toString()));
                 })
             })
             req.on('error', error => {
-                console.error(error)
+                reject(error);
             })
             req.end()
         })
     }
 
-    async updateTemperature(id, temperature) {
+    async updateTemperature(token, id, temperature) {
         await this.login();
         return new Promise((resolve, reject) => {
             if (!id || !temperature)
                 throw new Error("Both ID and temperature named arguments must be set");
 
-            const data = JSON.stringify({"datapoint": {"value": temperature}})
+            const data = JSON.stringify({"datapoint": {"value": temperature * 100}})
             const options = {
                 host: baseUrl,
                 port: 443,
@@ -173,7 +149,7 @@ class Index {
                 headers: {
                     'Content-Type': 'application/json',
                     'Content-Length': data.length,
-                    'Authorization': 'Bearer ' + this.token
+                    'Authorization': 'Bearer ' + token
                 }
             }
 
